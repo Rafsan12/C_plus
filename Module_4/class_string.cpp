a7cae7be9b38b22d0076ebf7c90e08a3a8a4e8ad
Rafsan12 <rafsan.web.2022@gmail.com>
2026-09-01 21:02:43 +0600

    Add class string example

---
+++ Module_4/class_string.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Person {
+public:
+    string name; 
+    int age;     
+
+    // Constructor
+    Person(string nm, int ag) {
+        name = nm; 
+        age = ag;  
+    }
+
+    
+    void displayInfo() {
+        cout << "Name: " << name << ", Age: " << age << endl;
+    }
+};
+
+int main() {
+    Person rafsan("Rafsan Ahmed", 27);
+    
+    
+    rafsan.displayInfo();
+      
+    return 0;
+}
